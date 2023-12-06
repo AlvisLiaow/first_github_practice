590fe5f816b57d82cc68cd73fa7d5c9f7adaa6de
AlvisLiaow <alvisliaow@gmail.com>
2023-12-05 20:59:06 -0500

    First commit to by new branch

--- practiceGitWithJava/src/practiceGitWithJava/GitPractice.java
+++ practiceGitWithJava/src/practiceGitWithJava/GitPractice.java
@@ -4,5 +4,6 @@ public class GitPractice {
 	public static void main(String[] args) {
 		System.out.println("Git practice, Hello world");
 		System.out.println("test");
+		System.out.println("Test again");
 	}
 }
