a70bced92beb193a6a530f2d61e30bf167bb9755
AlvisLiaow <alvisliaow@gmail.com>
2023-12-05 19:28:59 -0500

    update

--- practiceGitWithJava/src/practiceGitWithJava/GitPractice.java
+++ practiceGitWithJava/src/practiceGitWithJava/GitPractice.java
@@ -3,5 +3,6 @@ package practiceGitWithJava;
 public class GitPractice {
 	public static void main(String[] args) {
 		System.out.println("Git practice, Hello world");
+		System.out.println("test");
 	}
 }
